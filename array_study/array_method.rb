d95700fe1a145842796cc3d5a6408f518b7defdc
2587332053@qq.com <2587332053@qq.com>
2019-03-07 12:14:04 +0800

    add file

---
+++ array_study/array_method.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby -w 
+
+#array & other_array
+#返回一个新的数组，包含两个数组中共同的元素，没有重复。
+arr = Array(0..2)
+puts "#{arr}"
+other = Array(3..5)
+puts "#{other}"
+result = arr << other
+puts "#{result}"
+
